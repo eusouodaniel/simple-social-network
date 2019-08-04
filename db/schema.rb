1f94900a1d30017d4eb988b17c7cb586d531dbf4
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2019-08-03 21:04:14 -0300

    fix(): Fix on tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205225036) do
+ActiveRecord::Schema.define(version: 20190802225036) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
